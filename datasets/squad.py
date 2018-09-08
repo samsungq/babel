c559cc823deef1186c353a2a4337b84d0f8e7fa6
mynameisvinn <vin.tang@gmail.com>
2018-09-08 16:46:30 -0400

    indent error

--- datasets/squad.py
+++ datasets/squad.py
@@ -2,9 +2,9 @@ import wget
 import json
 
 def load_data():
-	url = 'https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json'
-	filename = wget.download(url)
+    url = 'https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json'
+    filename = wget.download(url)
 
-	with open(filename, 'r') as f:
-    	source_data = json.load(f)
+    with open(filename, 'r') as f:
+        source_data = json.load(f)
     return source_data
